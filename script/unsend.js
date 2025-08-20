79d2153a05c1b24900f2428784277c3d949f1bb4
KonanBot34 <konanai845@gmail.com>
2025-08-21 06:29:47 +0800

    Update unsend.js

--- script/unsend.js
+++ script/unsend.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "unsend",
   version: "1.0.0",
-  role: 0,
+  role: 2//admin,
   hasPrefix: true,
   aliases: ['unsent', 'remove', 'rm'],
   usage: 'Unsent [reply]',
